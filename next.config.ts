9e7316e3505cec0b6380c6001561a841098ea025
João Carlos Ressel <joao.resselti@gmail.com>
2025-11-25 19:18:22 -0500

    fix(build): Disable PWA build in development mode

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === "development";
 const nextConfig: NextConfig = {
   /* config options here */
 };
